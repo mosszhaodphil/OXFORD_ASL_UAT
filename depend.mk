885d54efbb3b2c8d0d83e3e947f90395f1b43019
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-03-25 08:14:27 +0000

    OXFORD_ASL with quasil for pv
    This version includes QUASIL for partial volume analysis

---
+++ depend.mk
@@ -0,0 +1,63 @@
+asl_file.o: asl_file.cc \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmatap.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmat.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/include.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/boolean.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/myexcept.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmatio.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimageall.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimage.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/lazy.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/lazyiterators.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/positerators.h \
+  /Users/mosszhao/Github/FSL_DEV/include/miscmaths/miscmaths.h \
+  /Users/mosszhao/Github/FSL_DEV/include/fslio/fslio.h \
+  /Users/mosszhao/Github/FSL_DEV/include/niftiio/nifti1_io.h \
+  /Users/mosszhao/Github/FSL_DEV/include/niftiio/nifti1.h \
+  /Users/mosszhao/Github/FSL_DEV/include/znzlib/znzlib.h \
+  /Users/mosszhao/Github/FSL_DEV/include/fslio/dbh.h \
+  /Users/mosszhao/Github/FSL_DEV/include/miscmaths/kernel.h \
+  /Users/mosszhao/Github/FSL_DEV/include/miscmaths/splinterpolator.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimageio.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/complexvolume.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimagefns.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/imfft.h \
+  /Users/mosszhao/Github/FSL_DEV/include/utils/tracer_plus.h \
+  /Users/mosszhao/Github/FSL_DEV/include/utils/time_tracer.h \
+  readoptions.h /Users/mosszhao/Github/FSL_DEV/include/utils/options.h \
+  /Users/mosszhao/Github/FSL_DEV/include/utils/log.h asl_functions.h
+asl_functions.o: asl_functions.cc asl_functions.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimageall.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimage.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmatap.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmat.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/include.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/boolean.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/myexcept.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/lazy.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/lazyiterators.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/positerators.h \
+  /Users/mosszhao/Github/FSL_DEV/include/miscmaths/miscmaths.h \
+  /Users/mosszhao/Github/FSL_DEV/include/fslio/fslio.h \
+  /Users/mosszhao/Github/FSL_DEV/include/niftiio/nifti1_io.h \
+  /Users/mosszhao/Github/FSL_DEV/include/niftiio/nifti1.h \
+  /Users/mosszhao/Github/FSL_DEV/include/znzlib/znzlib.h \
+  /Users/mosszhao/Github/FSL_DEV/include/fslio/dbh.h \
+  /Users/mosszhao/Github/FSL_DEV/include/miscmaths/kernel.h \
+  /Users/mosszhao/Github/FSL_DEV/include/miscmaths/splinterpolator.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimageio.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmatio.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/complexvolume.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/newimagefns.h \
+  /Users/mosszhao/Github/FSL_DEV/include/newimage/imfft.h
+readoptions.o: readoptions.cc readoptions.h \
+  /Users/mosszhao/Github/FSL_DEV/include/utils/options.h \
+  /Users/mosszhao/Github/FSL_DEV/include/utils/log.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmatap.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmat.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/include.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/boolean.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/myexcept.h \
+  /Users/mosszhao/Github/FSL_DEV/extras/include/newmat/newmatio.h \
+  /Users/mosszhao/Github/FSL_DEV/include/utils/tracer_plus.h \
+  /Users/mosszhao/Github/FSL_DEV/include/utils/time_tracer.h
